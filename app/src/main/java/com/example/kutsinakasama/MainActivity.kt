a3cdb54552d8a6b4402309ecf2cc1356823bc221
Martina Mari Andaya <martina.mari.andaya@gmail.com>
2025-10-13 10:55:03 +0800

    changes: - added bottom nav - added profile view - will add navController soon pa may error lng sa MainActivity.kt rn etc

--- app/src/main/java/com/example/kutsinakasama/MainActivity.kt
+++ app/src/main/java/com/example/kutsinakasama/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.example.kutsinakasama
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import com.example.kutsinakasama.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // Connect bottom nav to navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        // Hide bottom nav on edit profile
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.editProfileFragment) {
+                binding.bottomNavigationView.visibility = android.view.View.GONE
+            } else {
+                binding.bottomNavigationView.visibility = android.view.View.VISIBLE
+            }
+        }
     }
 }
