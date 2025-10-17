5ad6f2d2a3fe40dc6d1a33305ae222a6d3456c77
capnchrome <104608360+capnchrome@users.noreply.github.com>
2025-10-17 20:03:22 +0800

    Update RecipeActivity.kt

--- app/src/main/java/com/example/kutsinakasama/RecipeActivity.kt
+++ app/src/main/java/com/example/kutsinakasama/RecipeActivity.kt
@@ -1,23 +1,21 @@
 package com.example.kutsinakasama
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.kutsinakasama.databinding.EditProfileBinding
+import com.example.kutsinakasama.databinding.RecipeBinding
 
 class RecipeActivity : AppCompatActivity() {
 
-    private lateinit var binding: EditProfileBinding
+    private lateinit var binding: RecipeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.recipe)
 
-        val titleTextView = findViewById<TextView>(R.id.tvRecipeTitle)
-        titleTextView.text = "The Perfect Egg"
+        binding = RecipeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        binding.btnBack.setOnClickListener {
-            onBackPressedDispatcher.onBackPressed()
-        }
+        binding.tvRecipeTitle.text = "The Perfect Egg"
     }
 }
+
+
