909b21a9eda96fe16e1f615b280b875d74eb5f32
capnchrome <104608360+capnchrome@users.noreply.github.com>
2025-11-20 23:41:54 +0800

    Update RecipeResponse.kt

--- app/src/main/java/com/example/kutsinakasama/RecipeResponse.kt
+++ app/src/main/java/com/example/kutsinakasama/RecipeResponse.kt
@@ -1,5 +1,6 @@
 package com.example.kutsinakasama
 
+
 data class RecipeResponse(
     val id: Int,
     val title: String,
@@ -10,4 +11,13 @@ data class RecipeResponse(
 
 data class Ingredient(
     val original: String
+)
+data class RecipeSearchResponse(
+    val results: List<RecipePreview>
+)
+
+data class RecipePreview(
+    val id: Int,
+    val title: String,
+    val image: String
 )
